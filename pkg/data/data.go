d5da3c8cc01c067fc0137d4483ddfbcae6abcd24
Nguyen Hiep <nguyen.tph128@gmail.com>
2024-11-24 02:10:14 +0700

    chore: do something

--- pkg/data/data.go
+++ pkg/data/data.go
@@ -1,31 +1,122 @@
 package data
 
 import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"strings"
 	"time"
 )
 
+// -------- status
+
+type ItemStatus int
+
+const (
+	ItemStatusTodo ItemStatus = iota
+	ItemStatusDoing
+	ItemStatusDone
+	ItemStatusDeleted
+)
+
+var allStatus = []string{"todo", "doing", "done", "deleted"}
+
+func (i *ItemStatus) String() string {
+	return allStatus[*i]
+}
+
+func parseStatus(status string) (ItemStatus, error) {
+	for i := range allStatus {
+		if allStatus[i] == status {
+			return ItemStatus(i), nil
+		}
+	}
+	return ItemStatus(0), errors.New("invalid status")
+}
+
+func (i *ItemStatus) Scan(value any) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New("invalid type")
+	}
+
+	v, err := parseStatus(string(bytes))
+	if err != nil {
+		return errors.New("invalid status")
+	}
+
+	*i = v
+	return nil
+}
+
+func (i *ItemStatus) Value() (driver.Value, error) {
+	if i == nil {
+		return nil, nil
+	}
+	return i.String(), nil
+}
+
+func (i *ItemStatus) MarshalJSON() ([]byte, error) {
+	if i == nil {
+		return []byte("null"), nil
+	}
+
+	return []byte(fmt.Sprintf(`"%s"`, i.String())), nil
+}
+
+func (i *ItemStatus) UnmarshalJSON(data []byte) error {
+	str := strings.ReplaceAll(string(data), `\"`, "")
+
+	v, err := parseStatus(str)
+	if err != nil {
+		return err
+	}
+
+	*i = v
+	return nil
+}
+
+// -------- paging
+type DataPaging struct {
+	Page  int   `json:"page" form:"page"`
+	Limit int   `json:"limit" form:"limit"`
+	Total int64 `json:"total" form:"-"`
+}
+
+func (p *DataPaging) Process() {
+	if p.Page <= 0 {
+		p.Page = 1
+	}
+
+	if p.Limit <= 0 || p.Limit > 100 {
+		p.Limit = 10
+	}
+
+}
+
 // -------- todo
+
 type ToDoItem struct {
-	Id          int        `json:"id" gorm:"column:id;"`
-	Title       string     `json:"title" gorm:"column:title;"`
-	Description string     `json:"description" gorm:"column:description;"`
-	Status      string     `json:"status" gorm:"column:status;"`
-	CreatedAt   *time.Time `json:"created_at" gorm:"column:created_at;"`
-	UpdatedAt   *time.Time `json:"updated_at,omitempty" gorm:"column:updated_at;"`
+	Id          int         `json:"id" gorm:"column:id;"`
+	Title       string      `json:"title" gorm:"column:title;"`
+	Description string      `json:"description" gorm:"column:description;"`
+	Status      *ItemStatus `json:"status" gorm:"column:status;"`
+	CreatedAt   *time.Time  `json:"created_at" gorm:"column:created_at;"`
+	UpdatedAt   *time.Time  `json:"updated_at,omitempty" gorm:"column:updated_at;"`
 }
 
 func (ToDoItem) TableName() string {
 	return "todo_items"
 }
 
-type TodoItemCreation struct {
+type TodoItemCreate struct {
 	Id          int    `json:"-" gorm:"column:id"` // auto increment
 	Title       string `json:"title" gorm:"column:title"`
 	Description string `json:"description" gorm:"column:description"`
-	Status      string `json:"status" gorm:"column:status"`
+	Status      *ItemStatus `json:"status" gorm:"column:status"`
 }
 
-func (TodoItemCreation) TableName() string {
+func (TodoItemCreate) TableName() string {
 	return ToDoItem{}.TableName()
 }
 
--- pkg/handler/todo/handler.go
+++ pkg/handler/todo/handler.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ntphiep/go-todo-pg/pkg/data"
@@ -13,7 +12,7 @@ import (
 
 func CreateItem(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var dataItem data.ToDoItem
+		var dataItem data.TodoItemCreate
 
 		if err := c.ShouldBind(&dataItem); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
@@ -23,13 +22,6 @@ func CreateItem(db *gorm.DB) gin.HandlerFunc {
 		}
 
 		// preprocess title - trim all spaces
-		dataItem.Title = strings.TrimSpace(dataItem.Title)
-		if dataItem.Title == "" {
-			// default status
-			dataItem.Status = "todo"
-			return
-		}
-
 		if err := db.Create(&dataItem).Error; err != nil {
 			log.Println("Cannot create item:", err)
 			c.JSON(http.StatusInternalServerError, gin.H{
@@ -46,42 +38,35 @@ func CreateItem(db *gorm.DB) gin.HandlerFunc {
 
 func GetListOfItems(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// list items
+		var result []data.ToDoItem
+		var paging data.DataPaging
 
-		type DataPaging struct {
-			Page  int   `json:"page" form:"page"`
-			Limit int   `json:"limit" form:"limit"`
-			Total int64 `json:"total" form:"-"`
-		}
-
-		var paging DataPaging
 		if err := c.ShouldBind(&paging); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
 
-		if paging.Page <= 0 {
-			paging.Page = 1
-		}
+		paging.Process()
 
-		if paging.Limit <= 0 {
-			paging.Limit = 10
-		}
-
-		offset := (paging.Page - 1) * paging.Limit
-
-		var result []data.ToDoItem
+		db = db.Where("status <> ?", "deleted")
 
 		if err := db.Table(data.ToDoItem{}.TableName()).
 			Count(&paging.Total).
-			Offset(offset).
+			Offset((paging.Page - 1) * paging.Limit).
 			Order("id desc").
 			Find(&result).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"data": result})
+		c.JSON(http.StatusOK, gin.H{
+			"data":   result,
+			"paging": paging,
+		})
 	}
 }
 
@@ -156,17 +141,26 @@ func DeleteItemById(db *gorm.DB) gin.HandlerFunc {
 		id, err := strconv.Atoi(c.Param("id"))
 
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
 
 		if err := db.Table(data.ToDoItem{}.TableName()).
 			Where("id = ?", id).
-			Delete(nil).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			Updates(map[string]interface{}{
+				"status": "deleted",
+			}).
+			Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"data": true})
+		c.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
 	}
 }
